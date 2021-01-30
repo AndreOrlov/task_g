7f7d28a9e88473aa713d6ed2e27cd3c392c5e907
Алексей Овчинников <alexey.ovchinnikov@yandex.ru>
2021-01-30 12:53:41 +0300

    initial clean project

---
+++ lib/geo_tasks.ex
@@ -0,0 +1,18 @@
+defmodule GeoTasks do
+  @moduledoc """
+  Documentation for `GeoTasks`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeoTasks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
